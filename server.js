e4d8ab1576c7d093267347405966627bb2f7e02e
BennyhinnTitus <bennyhinntitus@gmail.com>
2025-06-22 15:46:42 +0530

    updated file structure

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.js';
+import authRoutes from '../routes/auth.js';
 import userRoutes from './routes/user.js';
 import progresssRoutes from './routes/progress.js';
 import cors from 'cors';
