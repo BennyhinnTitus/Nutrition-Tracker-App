f20275051bebb5fe7b34f241b797f3e10ce8a3ea
BennyhinnTitus <bennyhinntitus@gmail.com>
2025-06-28 23:42:27 +0530

    update date's datatype

--- models/db.js
+++ models/db.js
@@ -7,6 +7,7 @@ const db = await mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  dateStrings: ['DATE'],
 });
 
 const checkConnection = async () => {
